03e83b6739f04cbebf21670ce0865417c05432df
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-10-15 18:04:43 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -16,17 +16,17 @@ function crazy() {
   };
   thisIsCrazy();
 
-  
+
 }
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
+  // var name = "Cricky";
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
+
+  
 }
